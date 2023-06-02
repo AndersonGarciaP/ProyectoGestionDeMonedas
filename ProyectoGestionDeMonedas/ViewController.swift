c654238d2c3ca538dfda162c183308cf7ce65e7f
alexgp94 <alexcgp94@gmail.com>
2023-06-01 23:54:14 -0500

    Initial Commit

---
+++ ProyectoGestionDeMonedas/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ProyectoGestionDeMonedas
+//
+//  Created by Alexander Claude Garcia Palacios on 1/06/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ ProyectoGestionDeMonedasUITests/ProyectoGestionDeMonedasUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  ProyectoGestionDeMonedasUITestsLaunchTests.swift
+//  ProyectoGestionDeMonedasUITests
+//
+//  Created by Alexander Claude Garcia Palacios on 1/06/23.
+//
+
+import XCTest
+
+final class ProyectoGestionDeMonedasUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
